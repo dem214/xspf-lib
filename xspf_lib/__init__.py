7943f47080f0432a42537e84431655191d35e8dc
Dzmitry Izaitka <dzmasu@tut.by>
2020-03-11 15:02:59 +0300

    clearing builder and xml

--- xspf_lib/__init__.py
+++ xspf_lib/__init__.py
@@ -239,35 +239,7 @@ class Track():
         tuples like `[Extension, ...]`
 
         """
-        if isinstance(location, URI):
-            self.location = [location]
-        else:
-            self.location = location
-        if isinstance(identifier, URI):
-            self.identifier = [identifier]
-        else:
-            self.identifier = identifier
-        self.title = title
-        self.creator = creator
-        self.annotation = annotation
-        self.info = info
-        self.image = image
-        self.album = album
-        if trackNum is not None:
-            if not int(trackNum) > 0:
-                raise ValueError("""trackNum must be greater than zero""")
-            self.trackNum = int(trackNum)
-        else:
-            self.trackNum = None
-        if duration is not None:
-            if int(duration) < 0:
-                raise ValueError("""duration must be a non negative integer""")
-            self.duration = duration
-        else:
-            self.duration = None
-        self.link = list(link)
-        self.meta = list(meta)
-        self.extension = list(extension)
+        _Builder().build_track(self, locals())
 
     __slots__ = ('location', 'identifier', 'title', 'creator', 'annotation',
                  'info', 'image', 'album', 'trackNum', 'duration', 'link',
@@ -289,34 +261,7 @@ class Track():
     @property
     def xml_element(self) -> ET.Element:
         """Create `xml.ElementTree.Element` of the track."""
-        track = ET.Element('track')
-        if self.location is not None:
-            for loc in self.location:
-                ET.SubElement(track, 'location').text = \
-                    str(urlparse.quote(loc, safe='/:'))
-        if self.identifier is not None:
-            for id in self.identifier:
-                ET.SubElement(track, 'identifier').text = str(id)
-        if self.title is not None:
-            ET.SubElement(track, 'title').text = str(self.title)
-        if self.creator is not None:
-            ET.SubElement(track, 'creator').text = str(self.creator)
-        if self.annotation is not None:
-            ET.SubElement(track, 'annotation').text = str(self.annotation)
-        if self.info is not None:
-            ET.SubElement(track, 'info').text = str(self.info)
-        if self.image is not None:
-            ET.SubElement(track, 'image').text = str(self.image)
-        if self.album is not None:
-            ET.SubElement(track, 'album').text = str(self.album)
-        if self.trackNum is not None:
-            ET.SubElement(track, 'trackNum').text = str(self.trackNum)
-        if self.duration is not None:
-            ET.SubElement(track, 'duration').text = str(self.duration)
-        track.extend(link._to_element() for link in self.link)
-        track.extend(meta._to_element() for meta in self.meta)
-        track.extend([extension._to_element() for extension in self.extension])
-        return track
+        return _XML_Builder(self).build_track()
 
     def xml_string(self) -> str:
         """Return XML representation of track."""
@@ -398,40 +343,7 @@ class Playlist(UserList):
     @property
     def xml_element(self) -> ET.Element:
         """Return `xml.ElementTree.Element` of the playlist."""
-
-        playlist = ET.Element('playlist', {'version': "1",
-                                           'xmlns': NS['xspf']})
-
-        # Setting bunch of parameters.
-        for parameter in ['title', 'creator', 'annotation']:
-            attr = self.__dict__[parameter]
-            if attr is not None:
-                ET.SubElement(playlist, parameter).text = str(attr)
-
-        # Setting bunch of uri parameters.
-        for uri_param in ['info', 'location', 'identifier', 'image']:
-            attr = self.__dict__[uri_param]
-            if attr is not None:
-                ET.SubElement(playlist, uri_param).text = str(quote(attr))
-
-        ET.SubElement(playlist, 'date').text = self.date.isoformat()
-        if self.license is not None:
-            ET.SubElement(playlist, 'license').text = str(quote(self.license))
-        if len(self.attribution) > 0:
-            attribution = ET.SubElement(playlist, 'attribution')
-            for attr in self.attribution[0:9]:
-                if attr.location is not None:
-                    ET.SubElement(attribution, 'location').text = attr.location
-                if attr.identifier is not None:
-                    ET.SubElement(attribution, 'identifier').text = \
-                        attr.identifier
-        playlist.extend(link._to_element() for link in self.link)
-        playlist.extend(meta._to_element() for meta in self.meta)
-        playlist.extend(
-            [extension._to_element() for extension in self.extension])
-        ET.SubElement(playlist, 'trackList').extend(
-            (track.xml_element for track in self.trackList))
-        return playlist
+        return _XML_Builder(self).build_playlist()
 
     @property
     def xml_eltree(self) -> ET.ElementTree:
@@ -462,6 +374,216 @@ class Playlist(UserList):
 
     def _to_attribution(self) -> Attribution:
         return Attribution(location=self.location, identifier=self.identifier)
+
+
+class _Builder():
+    def build_track(self, entity, parameters: Dict):
+        self.entity = entity
+        self.parameters = parameters
+
+        self.add_location()
+        self.add_identifier()
+        self.add_title()
+        self.add_creator()
+        self.add_annotation()
+        self.add_info()
+        self.add_image()
+        self.add_album()
+        self.add_trackNum()
+        self.add_duration()
+        self.add_link()
+        self.add_meta()
+        self.add_extension()
+
+        return self.entity
+
+    def add_location(self):
+        if isinstance(self.parameters['location'], URI):
+            self.entity.location = [self.parameters['location']]
+        else:
+            self.entity.location = self.parameters['location']
+
+    def add_identifier(self):
+        if isinstance(self.parameters['identifier'], URI):
+            self.entity.identifier = [self.parameters['identifier']]
+        else:
+            self.entity.identifier = self.parameters['identifier']
+
+    def add_title(self):
+        self.add_simple_parameter('title')
+
+    def add_creator(self):
+        self.add_simple_parameter('creator')
+
+    def add_annotation(self):
+        self.add_simple_parameter('annotation')
+
+    def add_info(self):
+        self.add_simple_parameter('info')
+
+    def add_image(self):
+        self.add_simple_parameter('image')
+
+    def add_album(self):
+        self.add_simple_parameter('album')
+
+    def add_trackNum(self):
+        trackNum = self.parameters['trackNum']
+        if trackNum is not None:
+            if not int(trackNum) > 0:
+                raise ValueError("""trackNum must be greater than zero""")
+            self.entity.trackNum = int(trackNum)
+        else:
+            self.entity.trackNum = None
+
+    def add_duration(self):
+        duration = self.parameters['duration']
+        if duration is not None:
+            if int(duration) < 0:
+                raise ValueError("""duration must be a non negative integer""")
+            self.entity.duration = duration
+        else:
+            self.entity.duration = None
+
+    def add_link(self):
+        self.entity.link = list(self.parameters['link'])
+
+    def add_meta(self):
+        self.entity.meta = list(self.parameters['meta'])
+
+    def add_extension(self):
+        self.entity.extension = list(self.parameters['extension'])
+
+    def add_simple_parameter(self, parameter_name: str):
+        self.entity.__setattr__(parameter_name,
+                                self.parameters[parameter_name])
+
+
+class _XML_Builder:
+    def __init__(self, entity):
+        self.entity = entity
+
+    def build_track(self):
+        self.xml_element = ET.Element('track')
+
+        self.add_locations()
+        self.add_identifiers()
+        self.add_title()
+        self.add_creator()
+        self.add_annotation()
+        self.add_info()
+        self.add_image()
+        self.add_album()
+        self.add_trackNum()
+        self.add_duration()
+        self.add_links()
+        self.add_metas()
+        self.add_extensions()
+
+        return self.xml_element
+
+    def build_playlist(self):
+        self.xml_element = ET.Element('playlist', {'version': "1",
+                                      'xmlns': NS['xspf']})
+        self.add_title()
+        self.add_creator()
+        self.add_annotation()
+        self.add_info()
+        self.add_location()
+        self.add_identifier()
+        self.add_image()
+        self.add_date()
+        self.add_license()
+        self.add_attribution()
+        self.add_links()
+        self.add_metas()
+        self.add_extensions()
+        self.add_trackList()
+
+        return self.xml_element
+
+    def add_locations(self):
+        if self.entity.location is not None:
+            for loc in self.entity.location:
+                ET.SubElement(self.xml_element, 'location').text = \
+                    str(urlparse.quote(loc, safe='/:'))
+
+    def add_identifiers(self):
+        if self.entity.identifier is not None:
+            for id in self.entity.identifier:
+                ET.SubElement(self.xml_element, 'identifier').text = str(id)
+
+    def add_license(self):
+        self.add_simple_subelement('license')
+
+    def add_attribution(self):
+        if len(self.entity.attribution) > 0:
+            attribution = ET.SubElement(self.xml_element, 'attribution')
+            for attr in self.entity.attribution[0:9]:
+                if attr.location is not None:
+                    ET.SubElement(attribution, 'location').text = attr.location
+                if attr.identifier is not None:
+                    ET.SubElement(attribution, 'identifier').text = \
+                        attr.identifier
+
+    def add_trackList(self):
+        ET.SubElement(self.xml_element, 'trackList').extend(
+            track.xml_element for track in self.entity.trackList
+        )
+
+    def add_title(self):
+        self.add_simple_subelement('title')
+
+    def add_creator(self):
+        self.add_simple_subelement('creator')
+
+    def add_annotation(self):
+        self.add_simple_subelement('annotation')
+
+    def add_info(self):
+        self.add_simple_subelement('info')
+
+    def add_image(self):
+        self.add_simple_subelement('image')
+
+    def add_album(self):
+        self.add_simple_subelement('album')
+
+    def add_trackNum(self):
+        self.add_simple_subelement('trackNum')
+
+    def add_duration(self):
+        self.add_simple_subelement('duration')
+
+    def add_links(self):
+        self.add_iterable_parameter('link')
+
+    def add_metas(self):
+        self.add_iterable_parameter('meta')
+
+    def add_extensions(self):
+        self.add_iterable_parameter('extension')
+
+    def add_location(self):
+        self.add_simple_subelement('location')
+
+    def add_identifier(self):
+        self.add_simple_subelement('identifier')
+
+    def add_date(self):
+        ET.SubElement(self.xml_element, 'date').text = \
+            self.entity.date.isoformat()
+
+    def add_simple_subelement(self, parameter_name: str):
+        parameter = getattr(self.entity, parameter_name, None)
+        if parameter is not None:
+            ET.SubElement(self.xml_element, parameter_name).text = \
+                str(parameter)
+
+    def add_iterable_parameter(self, parameter_name: str):
+        parameter_iter: int = getattr(self.entity, parameter_name)
+        self.xml_element.extend(parameter._to_element()
+                                for parameter in parameter_iter)
 
 
 class _Parser():

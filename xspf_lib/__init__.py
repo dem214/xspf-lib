95edd7061a739250e37f02c093784739eaa03575
Dzmitry Izaitka <dem214overlord@gmail.com>
2023-05-26 12:56:03 +0300

    dynamic version

--- xspf_lib/__init__.py
+++ xspf_lib/__init__.py
@@ -1,9 +1,9 @@
 """Module helps to work with xspf playlists."""
 
 __all__ = ["Playlist", "Track", "Attribution", "Extension", "Link", "Meta", "URI"]
-
 import xml.etree.ElementTree as Et
 
+from ._version import __version__
 from .constants import XML_NAMESPACE
 from .elements import Attribution, Extension, Link, Meta, Playlist, Track
 from .types import URI
